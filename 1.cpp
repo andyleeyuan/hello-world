76607c1d8e23916cd8d873000a62ead25faef89d
andyleeyuan <andyleeyuan@gmail.com>
2018-06-20 17:17:41 +0800

    second

--- 1.cpp
+++ 1.cpp
@@ -5,17 +5,17 @@ using namespace std;
 
 int main()
 {
-    string str1,str2,str3;
+    string str1,str2;
     
     
     cout<<"Please enter two strings:\n";
     getline(cin,str1);
     getline(cin,str2);
-    getline(cin,str3)
     
     
     
-    str1=str3+str1+str2.substr(0,3);
+    
+    str1=str1+str2.substr(0,3);
     
     
     cout<<"string1="<<str1;
